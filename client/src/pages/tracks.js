5a71a79c4211eca3c2a5ee9740b6ad91de4a8aad
samroodAli <devsamrood@gmail.com>
2021-12-30 01:44:55 +0530

    Client:Use hook useQuery to fetch data from server with loading,error and data states

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,29 +1,40 @@
-import React from 'react';
-import { Layout } from '../components';
-import {gql} from "@apollo/client"
+import React from "react";
+import { Layout } from "../components";
+import { gql, useQuery } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+import QueryResults from "../components/query-result";
 
 export const TRACKS = gql`
   query getTracks {
-  tracksForHome {
-    id
-    title
-    thumbnail
-    length
-    modulesCount
-    author {
+    tracksForHome {
       id
-      name
-      photo
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
     }
   }
-}
-`
+`;
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+  return (
+    <Layout grid>
+      <QueryResults error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard track={track} key={track.id} />
+        ))}
+      </QueryResults>
+    </Layout>
+  );
 };
 
 export default Tracks;
--- server/src/index.js
+++ server/src/index.js
@@ -1,32 +1,31 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require("./schema")
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
 
 const mocks = {
   Query: () => ({
-    tracksForHome: [...new Array(5)]
+    tracksForHome: [...new Array(8)],
   }),
   Track: () => ({
-    id: () => 'track_01',
-    title: () => 'Astro Kitty, Space Explorer',
+    id: () => "track_01",
+    title: () => "Astro Kitty, Space Explorer",
     author: () => {
       return {
-        name: 'Grumpy Cat',
+        name: "Grumpy Cat",
         photo:
-          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg'
+          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
       };
     },
     thumbnail: () =>
-      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
     length: () => 1210,
-    modulesCount: () => 6
-  })
+    modulesCount: () => 6,
+  }),
 };
-
 
 const server = new ApolloServer({
   typeDefs,
-  mocks
-})
+  mocks,
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
