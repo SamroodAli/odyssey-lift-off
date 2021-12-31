c95494ea978816332507a7e6726f4f3ae9d00bb6
samroodAli <devsamrood@gmail.com>
2021-12-31 21:49:08 +0530

    Client:Get track query from apollo studio explorer for fetching track details using useQuery and gql

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -2,6 +2,31 @@ import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import { Layout, QueryResult } from "../components";
 
+export const GET_TRACK = gql`
+  query getTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      length
+
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      modulesCount
+      description
+      numberOfViews
+      modules {
+        id
+        length
+        title
+      }
+    }
+  }
+`;
+
 const Track = ({ trackId }) => {
   return <Layout></Layout>;
 };
